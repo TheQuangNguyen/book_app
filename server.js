fd6a7ac26dd33a0953d8e0cb880333eb0304ab97
Benjamin Clark <benisin@gmail.com>
2019-07-23 11:21:34 -0700

    added a POST route for /searches. show.html renders results

--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 'use strict';
 
+// #region ---------- SETUP
+
+
 const express = require('express');
 const app = express();
 const superagent = require('superagent');
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.listen(PORT, () => {
+  console.log('Listening on PORT: ', PORT);
+})
+
+// #endregion SETUP
+
+
+function Book(info) {
+  this.coverImage = info.volumeInfo.imageLinks.thumbnail.replace(/^http:\/\//i, 'https://') || 'https://i.imgur.com/J5LVHEL.jpg';
+  this.title = info.volumeInfo.title || 'Title not available';
+  this.authors = info.volumeInfo.authors || ['Author not available'];
+  this.summary = info.volumeInfo.description || 'Summary not available'
+}
+
+
+// #region ---------- ROUTE HANDLERS
+
+app.post('/searches', (req, res) => {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+  // check if title or author is selected
+  let search = req.body.search[0].replace(/\s/g, '+');
+  if (req.body.search[1] === 'title') { url += `+intitle:${search}`; }
+  if (req.body.search[1] === 'author') { url += `+inauthor:${search}`; }
+  console.log(url);
+  superagent.get(url)
+    .then(result => result.body.items.slice(0, 10).map(bookInfo => new Book(bookInfo)))
+    .then(bookArr => res.render('pages/searches/show', { searchResults: bookArr }));
+
+});
 
 app.get('/', (req, res) => {
   res.render('./pages/index');
@@ -14,6 +51,6 @@ app.get('/', (req, res) => {
 
 app.get('*', (req, res) => res.status(404).send('Error. This route does not exist!!!'));
 
-app.listen(PORT, () => {
-  console.log('Listening on PORT: ', PORT);
-})
+// #endregion ROUTE HANDLERS
+
+
