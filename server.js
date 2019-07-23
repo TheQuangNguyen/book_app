1d209ff386a35aef12be19e4460aa66b7b4dbd49
Quang Nguyen <quangn2595@gmail.com>
2019-07-23 09:17:13 -0700

    getting things ready

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 'use strict';
 
+const express = require('express');
+const app = express();
+const superagent = require('superagent');
+const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/hello', (req, res) => { 
+  res.render('./pages/index');
+})
+
+app.get('*', (req, res) => res.status(404).send('Error. This route does not exist!!!'));
+
+app.listen(PORT, () => { 
+  console.log('Listening on PORT: ', PORT);
+})
