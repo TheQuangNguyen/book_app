f6298bf4257edcd703722e19258746197d700ed6
Quang Nguyen <quangn2595@gmail.com>
2019-07-23 09:34:08 -0700

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/hello', (req, res) => { 
+app.get('/', (req, res) => { 
   res.render('./pages/index');
 })
 
